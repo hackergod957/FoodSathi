be15a24d031e5308073c206c112bf723d5f4b4f4
hackergod957 <parrotos95@gmail.com>
2025-03-31 20:27:17 +0545

    hlo

--- components/SplashScreen.tsx
+++ components/SplashScreen.tsx
@@ -8,7 +8,9 @@ const SplashScreen = () => {
     <View className="flex-1 justify-center items-center">
       <Image source={splashScreen} className="absolute w-full z-0" />
       <Image source={logo} className="w-56 h-56" />
-      <Text className="text-[18px] mt-[22px] text-white  ">Hot food at your door wait no more !</Text>
+      <Text className="text-[18px] mt-[22px] text-white " style={{
+        fontFamily: 'Ketchup Manis Demo'
+      }}>Hot food at your door wait no more !</Text>
     </View>
   );
 };
