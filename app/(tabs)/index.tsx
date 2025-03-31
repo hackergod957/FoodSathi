0349bbdf489f52652ee2695fb6037333034dadd3
hackergod957 <parrotos95@gmail.com>
2025-03-31 20:13:23 +0545

    done

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,21 @@
 import SplashScreen from "@/components/SplashScreen";
+import { useNavigation } from "expo-router";
 import { useEffect, useState } from "react";
 import { Text, View } from "react-native";
 export default function Index() {
   const [splashScreen, setSplashScreen] = useState<boolean>(true);
+  const Navigation = useNavigation();
   useEffect(() => {
+    Navigation.setOptions({
+      tabBarStyle: {display: splashScreen ? 'none' : 'flat'}
+    })
     const result = setTimeout(() => {
       setSplashScreen(false);
-    }, 1500);
+    }, 3000);
     return () => {
       clearTimeout(result);
     };
-  });
+  },[splashScreen,Navigation]);
 
   return <>{splashScreen ? <SplashScreen /> : <View></View>}</>;
 }
